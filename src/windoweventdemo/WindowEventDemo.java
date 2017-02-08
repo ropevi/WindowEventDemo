e1547e9388f1470b0dadb4d83f56b45b98097ed3
Roger Pedrós Villorbina <pedrooos.46@gmail.com>
2017-02-08 18:22:56 +0100

    Versio 0.2 del projecte WindowsEventDemo

--- src/windoweventdemo/WindowEventDemo.java
+++ src/windoweventdemo/WindowEventDemo.java
@@ -27,6 +27,9 @@ public class WindowEventDemo extends Frame
       Button btnClose = new Button("Close");
       add(btnClose);
       
+      btnClose.setActionCommand("CLOSE");
+      btnClose.addActionListener(this);
+
       btnCount.addActionListener(this);
         // btnCount fires ActionEvent to its registered ActionEvent listener
         // btnCount adds "this" object as an ActionEvent listener
@@ -47,10 +50,14 @@ public class WindowEventDemo extends Frame
    /** ActionEvent handler */
    @Override
    public void actionPerformed(ActionEvent evt) {
+      String command = evt.getActionCommand();
+        if (command.equals("CLOSE")){
+        System.exit(0);
+      }
       ++count;
       tfCount.setText(count + "");
    }
- 
+  
    /** WindowEvent handlers */
    // Called back upon clicking close-window button
    @Override
